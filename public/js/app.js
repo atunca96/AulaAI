1015eb53df3cc837bb36e9519d49bb1cc56ac970
atunc <atunc@student.edu>
2026-05-24 14:55:15 +0300

    Perf: Direct Google Translate TTS from browser - eliminates server middleman, correct language via tl= param

--- public/js/app.js
+++ public/js/app.js
@@ -51,7 +51,7 @@ function _getLangCode(lang) {
 
 function _getTTSUrl(text, lang) {
   const tl = _getLangCode(lang);
-  return `/api/tts?text=${encodeURIComponent(text)}&lang=${encodeURIComponent(lang)}`;
+  return `https://translate.google.com/translate_tts?ie=UTF-8&q=${encodeURIComponent(text)}&tl=${tl}&client=tw-ob`;
 }
 
 // Preload: create Audio objects that start buffering immediately
